e43dfbf5de8adfe2be9e4c5390ef813e3927d3de
omer gokturk <omergokturktc@gmail.com>
2025-12-15 23:34:41 +0300

    feat: axios ve ortam değişkenleri ile proje kurulumu

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log(process.env.EXPO_PUBLIC_API_BASE_URL);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
